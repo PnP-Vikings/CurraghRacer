51f9192e41cc571b8b47acfcacd5a3b28e490df5
frankiemc15 <38690356+frankiemc15@users.noreply.github.com>
2026-04-14 02:07:05 +0100

    Traffic Warden game
    Rain & Roadworks sounds added

---
+++ Assets/Scripts/MiniGames/TrafficWardenMinigame/TrafficWardenAudio.cs
@@ -0,0 +1,53 @@
+using FMOD.Studio;
+using UnityEngine;
+
+public class TrafficWardenAudio : MonoBehaviour
+{
+    [SerializeField] TrafficWardenMinigameController trafficWardenMinigameController;
+    private PLAYBACK_STATE rainPlaybackState;
+    private PLAYBACK_STATE roadworksPlaybackState;
+
+    void Update()
+    {
+        Debug.Log("Active event is " + trafficWardenMinigameController.activeEvent + " - AudioDebug");
+        
+
+        if (AudioManager.instance != null)
+        {
+            AudioManager.instance.rain.getPlaybackState(out rainPlaybackState);
+            AudioManager.instance.roadworks.getPlaybackState(out roadworksPlaybackState);
+
+            switch (trafficWardenMinigameController.activeEvent)
+            {
+                case TrafficEventType.None:
+                    MuteRainAndRoadworks();
+                    break;
+                case TrafficEventType.OldPerson:
+                    MuteRainAndRoadworks();
+                    break;
+                case TrafficEventType.Ambulance:
+                    MuteRainAndRoadworks();
+                    break;
+                case TrafficEventType.Rain:
+                    if(rainPlaybackState == PLAYBACK_STATE.STOPPED)
+                    {
+                        AudioManager.instance.rain.start();
+                    }
+                    AudioManager.instance.roadworks.stop(STOP_MODE.ALLOWFADEOUT);
+                    break;
+                case TrafficEventType.Roadworks:
+                    if (roadworksPlaybackState  == PLAYBACK_STATE.STOPPED)
+                    {
+                        AudioManager.instance.roadworks.start();
+                    }
+                    AudioManager.instance.rain.stop(STOP_MODE.ALLOWFADEOUT);
+                    break;
+            }
+        }
+    }
+    public void MuteRainAndRoadworks()
+    {
+        AudioManager.instance.rain.stop(STOP_MODE.ALLOWFADEOUT);
+        AudioManager.instance.roadworks.stop(STOP_MODE.ALLOWFADEOUT);
+    }
+}
