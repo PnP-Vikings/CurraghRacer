354750fa66d27d21c00d685f3142bd9fd6db9a12
frankiemc15 <38690356+frankiemc15@users.noreply.github.com>
2026-07-02 16:35:02 +0100

    Tutorial Audio
    minigame_Win now plays when the tutorial is complete

--- Assets/Scripts/Audio/RadioManager.cs
+++ Assets/Scripts/Audio/RadioManager.cs
@@ -50,7 +50,8 @@ public class RadioManager : MonoBehaviour
     {
         PlayRadioSong();                                                    // A random song plays
 
-        float randomNumber = Random.Range(85f, 190f);   // .Range(5f, 6f);  // A random number between the given numbers is assigned to the float 'randomNumber'
+        float randomNumber = Random.Range(5f, 6f);                          // A random number between the given numbers is assigned to the float 'randomNumber' (For Testing)
+        //float randomNumber = Random.Range(85f, 190f);                     // A random number between the given numbers is assigned to the float 'randomNumber' (For actual Build)
         Debug.Log("Song playing for " +  randomNumber + " Seconds - RadioDebug");
 
         yield return new WaitForSeconds(randomNumber);                      // Waits randomNumber seconds
---
+++ Assets/Scripts/Audio/TutorialAudio.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using UnityEngine;
+
+public class TutorialAudio : MonoBehaviour
+{
+    public IEnumerator PlayTutorialCompleteAudio()
+    {
+        if(AudioManager.instance != null)
+        {
+            yield return new WaitForSeconds(1.5f);
+            AudioManager.instance.miniGame_Win.start();
+        }
+    }
+}
