293187d58aa2fac9dccb5a5ef71ad25ac83fd476
frankiemc15 <38690356+frankiemc15@users.noreply.github.com>
2026-08-26 13:06:50 +0100

    Tutorial Guy code improved
    The tutorial guy audio now plays until the text has finished typing itself out or if the player hits next

--- Assets/Scripts/Audio/RadioManager.cs
+++ Assets/Scripts/Audio/RadioManager.cs
@@ -53,8 +53,8 @@ public class RadioManager : MonoBehaviour
     {
         PlayRadioSong();                                                    // A random song plays
 
-        float randomNumber = Random.Range(5f, 6f);                          // A random number between the given numbers is assigned to the float 'randomNumber' (For Testing)
-        //float randomNumber = Random.Range(85f, 190f);                     // A random number between the given numbers is assigned to the float 'randomNumber' (For actual Build)
+        //float randomNumber = Random.Range(5f, 6f);                          // A random number between the given numbers is assigned to the float 'randomNumber' (For Testing)
+        float randomNumber = Random.Range(85f, 190f);                     // A random number between the given numbers is assigned to the float 'randomNumber' (For actual Build)
         //Debug.Log("Song playing for " +  randomNumber + " Seconds - RadioDebug");
 
         yield return new WaitForSeconds(randomNumber);                      // Waits randomNumber seconds
