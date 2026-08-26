217b96cd0b4e96fde5b789ef5293870c4d6d29a3
frankiemc15 <38690356+frankiemc15@users.noreply.github.com>
2026-08-26 12:22:51 +0100

    Minor edits

--- Assets/Scripts/Audio/TutorialAudio.cs
+++ Assets/Scripts/Audio/TutorialAudio.cs
@@ -18,8 +18,8 @@ public class TutorialAudio : MonoBehaviour
         if (AudioManager.instance != null)
         {
             AudioManager.instance.miniGameProgression.start();
-            AudioManager.instance.miniGameProgression.setParameterByName("miniGameProgression Volume", 0.8f);
-            //Debug.Log("miniGameProgression called 0.8 volume - AudioDebug");
+            AudioManager.instance.miniGameProgression.setParameterByName("miniGameProgression Volume", 0.78f);
+            //Debug.Log("miniGameProgression called 0.78 volume - AudioDebug");
         }
     }
 
